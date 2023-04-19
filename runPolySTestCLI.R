35d68d30e014a44d68520f90203e44e7d335eca1
veitveit <veits@bmb.sdu.dk>
2023-04-19 19:43:41 +0200

    corrected error for multiple replicates and only one condition

--- runPolySTestCLI.R
+++ runPolySTestCLI.R
@@ -56,7 +56,7 @@ if (!is.integer(ColQuant)) {
 }
 if (ColQuant < 1) {
   stop("Parameter firstquantcol is not a positive integer number > 0",
-    call. = FALSE
+       call. = FALSE
   )
 }
 filename <- pars$csvfile
@@ -66,7 +66,7 @@ if (!file.exists(filename)) {
 delim <- pars$delim
 if (all(delim != c("tab", ";", ",", "|"))) {
   stop("No allowed delim parameter value out of ',', ';', '|' or 'tab'",
-    call. = FALSE
+       call. = FALSE
   )
 }
 decimal <- pars$decimal
@@ -117,8 +117,8 @@ if (delim == "tab") {
   delim <- "\t"
 }
 dat <- read.csv(filename,
-  header = is_header, sep = delim, dec = decimal,
-  stringsAsFactors = F
+                header = is_header, sep = delim, dec = decimal,
+                stringsAsFactors = F
 )
 rownames(dat) <- paste("feature", 1:nrow(dat))
 
@@ -149,11 +149,11 @@ if (!rep_grouped) {
 }
 cat("\nExperimental setup as read:")
 knitr::kable(matrix(colnames(dat),
-  ncol = NumCond, byrow = T,
-  dimnames = list(
-    rows = paste("Replicate", 1:NumReps),
-    cols = paste("Condition", 1:NumCond)
-  )
+                    ncol = NumCond, byrow = T,
+                    dimnames = list(
+                      rows = paste("Replicate", 1:NumReps),
+                      cols = paste("Condition", 1:NumCond)
+                    )
 ))
 
 tncol <- 0
@@ -167,17 +167,17 @@ if (!is.null(addInfo)) {
 # Normalize
 if (normalization == "median") {
   dat <- t(t(dat) -
-    colMedians(as.matrix(dat), na.rm = TRUE))
+             colMedians(as.matrix(dat), na.rm = TRUE))
 } else if (normalization == "mean") {
   dat <- t(t(dat) -
-    colMeans(as.matrix(dat), na.rm = TRUE))
+             colMeans(as.matrix(dat), na.rm = TRUE))
 } else if (normalization == "sum_abs") {
   dat <- t(t(2^dat) /
-    colSums(as.matrix(2^dat), na.rm = TRUE))
+             colSums(as.matrix(2^dat), na.rm = TRUE))
   dat <- log2(dat)
 } else {
   dat <- limma::normalizeBetweenArrays(dat,
-    method = normalization
+                                       method = normalization
   )
 }
 
@@ -199,7 +199,7 @@ lcs <- function(a, b) {
       L[v] <- ifelse(any(v == 1), 1, L[v - 1] + 1)
     }
     paste0(A[(-max(L) + 1):0 + which(L == max(L),
-      arr.ind = TRUE
+                                     arr.ind = TRUE
     )[1]], collapse = "")
   } else {
     return(NA)
@@ -222,46 +222,49 @@ for (i in 1:NumCond) {
 
 ####### Defining comparison for statistical tests
 # all vs. all or all vs. refCond?
-allComps <- NULL
-if (refCond > 0) {
-  allComps <- cbind(conditions[refCond], conditions[conditions != conditions[refCond]])
-} else {
-  for (i in 1:(NumCond - 1)) {
-    for (j in (i + 1):NumCond) {
-      allComps <- rbind(allComps, c(conditions[i], conditions[j]))
-    }
-  }
-}
-cat("\nAll pairwise comparison between conditions:")
-colnames(allComps) <- paste("Condition", c("A", "B"))
-rownames(allComps) <- paste("Comparison", seq_len(nrow(allComps)))
-knitr::kable(t(allComps))
 
-
-allComps <- unique(allComps)
-allComps <- allComps[allComps[, 1, drop = F] != allComps[, 2, drop = F], , drop = F]
-compNames <- paste(allComps[, 2], " vs ", allComps[, 1], sep = "")
-ncomps <- nrow(allComps)
-
-valComps <- matrix(NA, nrow = ncomps, ncol = ncol(allComps))
-for (i in seq_len(length(allComps))) {
-  valComps[i] <-
-    as.numeric(which(conditions == allComps[i]))
-}
-RR <- matrix(NA, ncol = ncomps * NumReps, nrow = 2)
-for (j in 1:ncomps) {
-  compCond <- valComps[j, 2]
-  refCond <- valComps[j, 1]
-  RR[1, seq(j, ncomps * NumReps, ncomps)] <-
-    seq(compCond, NumCond * NumReps, NumCond)
-  RR[2, seq(j, ncomps * NumReps, ncomps)] <-
-    seq(refCond, NumCond * NumReps, NumCond)
-}
-
-###### Run tests
-
+FullReg <- allComps <- NULL
 ## Check for only one replicate per condition of only 1 condition
 if (NumCond > 1 & NumReps > 1) {
+  
+  
+  if (refCond > 0) {
+    allComps <- cbind(conditions[refCond], conditions[conditions != conditions[refCond]])
+  } else {
+    for (i in seq_len(NumCond - 1)) {
+      for (j in (i + 1):NumCond) {
+        allComps <- rbind(allComps, c(conditions[i], conditions[j]))
+      }
+    }
+  }
+  
+  cat("\nAll pairwise comparison between conditions:")
+  colnames(allComps) <- paste("Condition", c("A", "B"))
+  rownames(allComps) <- paste("Comparison", seq_len(nrow(allComps)))
+  print(knitr::kable(t(allComps)))
+  
+  allComps <- unique(allComps)
+  allComps <- allComps[allComps[, 1, drop = F] != allComps[, 2, drop = F], , drop = F]
+  compNames <- paste(allComps[, 2], " vs ", allComps[, 1], sep = "")
+  ncomps <- nrow(allComps)
+  
+  valComps <- matrix(NA, nrow = ncomps, ncol = ncol(allComps))
+  for (i in seq_len(length(allComps))) {
+    valComps[i] <-
+      as.numeric(which(conditions == allComps[i]))
+  }
+  RR <- matrix(NA, ncol = ncomps * NumReps, nrow = 2)
+  for (j in 1:ncomps) {
+    compCond <- valComps[j, 2]
+    refCond <- valComps[j, 1]
+    RR[1, seq(j, ncomps * NumReps, ncomps)] <-
+      seq(compCond, NumCond * NumReps, NumCond)
+    RR[2, seq(j, ncomps * NumReps, ncomps)] <-
+      seq(refCond, NumCond * NumReps, NumCond)
+  }
+  
+  ###### Run tests
+  
   # for maybe later to include onlyLIMMA option
   testNames <- c("limma", "Miss test", "rank products", "permutation test", "t-test")
   MAData <- NULL
@@ -279,7 +282,7 @@ if (NumCond > 1 & NumReps > 1) {
     qNAvalues = matrix(1, ncol = ncomps, nrow = nrow(dat))
   )
   MissingStats <- MissingStatsDesign(dat, RR, NumCond, NumReps)
-
+  
   ### Preparing data for output
   LogRatios <- qvalues$lratios
   Pvalue <- cbind(
@@ -292,7 +295,7 @@ if (NumCond > 1 & NumReps > 1) {
   )
   Qvalue <- cbind(UnifyQvals(Qvalue, ncomps, 5), Qvalue)
   # print(head(LogRatios))
-
+  
   colnames(LogRatios) <- paste("log-ratios", compNames)
   colnames(Pvalue) <- paste(
     "p-values", rep(testNames, each = ncomps),
@@ -305,17 +308,17 @@ if (NumCond > 1 & NumReps > 1) {
   )
   fullData <- cbind(addInfo, dat)
   FullReg <- cbind(as.data.frame(fullData), LogRatios, Qvalue)
-
+  
   #### report some statistics
-
+  
   cat("------- summary of results --------\n")
   cat("Number of differentially regulated features with FDR < 0.01\n")
   regMatr <- matrix(colSums(Qvalue < 0.01, na.rm = TRUE),
-    ncol = length(testNames2)
+                    ncol = length(testNames2)
   )
   colnames(regMatr) <- testNames2
   rownames(regMatr) <- compNames
-  knitr::kable(regMatr)
+  print(knitr::kable(regMatr))
 } else {
   cat("!!!  Warning: Only one condition or one replicate per condition, no statistical tests performed  !!!\n")
   FullReg <- cbind(as.data.frame(addInfo), dat)
