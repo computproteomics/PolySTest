b4631bc42bd751a96a4ebd7a62f7f972ac5f67f7
veitveit <veits@bmb.sdu.dk>
2024-03-29 10:05:27 +0100

    added library call

--- R/liver_example.R
+++ R/liver_example.R
@@ -9,6 +9,7 @@
 #' @source Protein expressions from the livers of mice fed with different diets: high fat,
 #' TTA component, Fish oil and TTA + fish oil
 #' @name liver_example
+#' @return A SummarizedExperiment object
 #' @examples
 #' data(liver_example)
 NULL
